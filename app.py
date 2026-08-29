06ad95a76351c9767742564887d1d658b7d7ec96
adityapawar5480-source <adityapawar5480@gmail.com>
2026-08-29 14:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,13 @@ if not st.session_state.logged_in:
             else:
                 st.error("Invalid Credentials! (Default: admin / 1234)")
     st.button("📞 Admin Support & Help")
+    st.info(
+                "If you forgot your password or need assistance, please reach out to the administrator:\n\n"
+                "**Admins:** Aditya Pawar, Aadarsh Raj, Anuj Kumar\n"
+                "📧 **Email:** adityapawar.cse25@satyug.edu.in\n"
+                "📱 **Phone:** +91 9289863106\n"
+                "💬 **Office:** Satyug Darshan Institute of Engineering & Technology, FBD"
+            )
     
 
 # ---------------------------------------------------------
