790e7d34e392a4e2a493e7c894ca8712e11ca94d
adityapawar5480-source <adityapawar5480@gmail.com>
2026-09-16 19:34:36 +0530

    Refactor app.py for improved structure and features

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if not st.session_state.logged_in:
                 "If you forgot your password or need assistance, please reach out to the administrator:\n\n"
                 "👨‍💼 **Admins:**    Aditya Pawar, Aadarsh Raj  \n"
                 "📧 **Email:**   adityapawar.cse25@satyug.edu.in  \n"
-                "📱 **Phone:**   +91 9289863106  \n"
+                "📱 **Phone:**   +91 9289863106, +91 9911340312 \n"
                 "💬 **Office:**  Satyug Darshan Institute of Engineering & Technology, FBD"
             )
 
