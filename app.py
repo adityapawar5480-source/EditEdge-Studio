efc6c3c287731fe6965d6bb29505820c719d20f4
adityapawar5480-source <adityapawar5480@gmail.com>
2026-09-16 19:37:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,8 @@ if not st.session_state.logged_in:
                 "If you forgot your password or need assistance, please reach out to the administrator:\n\n"
                 "👨‍💼 **Admins:**    Aditya Pawar, Aadarsh Raj  \n"
                 "📧 **Email:**   adityapawar.cse25@satyug.edu.in  \n"
-                "📱 **Phone:**   +91 9289863106 \n"
-                "💬 **Office:**  Satyug Darshan Institute of Engineering & Technology, FBD"
+                "📱 **Phone:**   +91 9289863106  \n"
+                "💬 **Office:**  Satyug Darshan Institute of Engineering & Technology, FBD"  \n"
             )
 
 # ---------------------------------------------------------
